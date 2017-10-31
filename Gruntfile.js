f6c8ce08193d12f7a77ec7d90ae302cb2fd80f7f
Paul Wardrip <pwardrip@gmail.com>
2017-10-31 00:48:28 -0400

    Dist files, bower cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,27 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     grunt.initConfig({
+        copy: {
+            dots: {
+                files: [
+                    {
+                        expand: true,
+                        src: ["src/dots.js"],
+                        dest: "dist/",
+                        filter: 'isFile',
+                        flatten: true
+                    }
+                ]
+            }
+        },
+
         concat: {
             dots: {
                 files: {
-                    'dist/dots.js': [
+                    'dist/dots-bundle.js': [
                         "bower_components/auto-lightbox/auto-lightbox.js",
                         "bower_components/centerize/centerize.js",
                         "src/dots.js"
@@ -20,7 +35,8 @@ module.exports = function(grunt) {
         uglify: {
             dots: {
                 files: {
-                    'dist/dots.min.js': ['dist/dots.js']
+                    'dist/dots-bundle.min.js': ['dist/dots-bundle.js'],
+                    'dist/dots.min.js': ['src/dots.js']
                 }
             }
         },
@@ -32,4 +48,6 @@ module.exports = function(grunt) {
             }
         }
     });
+
+    grunt.registerTask("default", ["copy", "concat", "uglify"]);
 };
